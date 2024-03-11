27662bd1d8e79b3ff12ddab8e80d536d12c3bca8
Amaro Ferraris <amaroferraris@gmail.com>
2024-03-11 20:53:40 -0300

    Ready to push (next: fix generateMessageMaca bug)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Welcome!
+============================
+My message generator
+============================
+This is an app to generate the messages I send to my music students every 1st of the month to let them know how much they have to pay me.
+
+The main future goal is to give the user the possibility to create his own messages (and not to use mine, of course).
--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const inputMesElement = document.getElementById('mes');
 const inputDiaElement = document.getElementById('dia');
 const inputNombreElement = document.getElementById('nombre');
+
 const inputPrecioElement = document.getElementById('precio');
+const inputPrecioCasaElement = document.getElementById('precio-casa');
+
 const inputClasesElement = document.getElementById('clases');
 const inputCasaElement = document.getElementById('casa');
+
 const btnGenerar = document.getElementById('btn-generar');
+const btnGenerarMaca = document.getElementById('btn-generar-maca');
 const btnBorrar = document.getElementById('btn-borrar');
+
 const total = document.getElementById('total');
 const output = document.getElementById('output');
 
 // ID
 let idCounter = 1
-
-const inputNombre = inputNombreElement.value;
-
 
 // PRECIO MAYOR o IGUAL A 0
 inputPrecioElement.addEventListener('input', function () {
@@ -21,6 +24,7 @@ inputPrecioElement.addEventListener('input', function () {
         inputPrecioElement.value = 0;
     }
 });
+
 
 // BOTÓN GENERAR
 btnGenerar.addEventListener('click', (e) => {
@@ -33,7 +37,7 @@ btnGenerar.addEventListener('click', (e) => {
     const inputNombre = inputNombreElement.value;
     const inputPrecio = inputPrecioElement.value;
     const inputClases = inputClasesElement.value;
-    const inputCasa = inputCasaElement.value;
+
 
     if (inputMes && inputDia && inputNombre && inputPrecio && inputClases) {
 
@@ -45,7 +49,33 @@ btnGenerar.addEventListener('click', (e) => {
             inputNombreElement.value = '';
 
         } else {
-            console.log('Hola maca');
+
+            inputCasaElement.style.display = 'inline';
+            inputPrecioCasaElement.style.display = 'inline';
+            btnGenerar.style.display = 'none';
+            btnGenerarMaca.style.display = 'inline';
+
+            btnGenerarMaca.addEventListener('click', (e) => {
+
+                e.preventDefault();
+
+                const inputPrecioCasa = inputPrecioCasaElement.value;
+                const inputCasa = inputCasaElement.value;
+
+
+                const message = generateMessageMaca(inputMes, inputDia, inputPrecio, inputNombre, inputClases, inputPrecioCasa, inputCasa, idCounter);
+
+                total.textContent = parseInt(total.textContent) + (parseInt(inputPrecio) * parseInt(inputClases) + (parseInt(inputPrecioCasa) * parseInt(inputCasa)));
+
+                inputNombreElement.value = '';
+                inputClasesElement.value = '';
+                inputCasaElement.style.display = 'none';
+                inputPrecioCasaElement.style.display = 'none';
+                btnGenerarMaca.style.display = 'none';
+                btnGenerar.style.display = 'inline';
+
+            })
+
         }
     }
 });
@@ -54,8 +84,8 @@ btnGenerar.addEventListener('click', (e) => {
 function generateMessage(mes, dia, precio, nombre, clases, id) {
     const message = `
         <div class="output-container">
-            <br>
-            <h3>${nombre.toUpperCase()}</h3>
+        <br>
+        <h3>${nombre.toUpperCase()}</h3>
 <p class="texto" id="p-${id}">Buen día ${nombre.charAt(0).toUpperCase()}${nombre.slice(1).toLowerCase()}, ¿cómo estás?
 <br><br>
 Te paso el plan de las clases de ${mes}:<br>
@@ -69,11 +99,36 @@ Cualquier cosa avisame, ¡nos vemos el ${dia}!</p>
             </div><br>
         </div>
     `;
-
     output.innerHTML += message;
-
     idCounter++
+    return;
+}
 
+// When I click on BORRAR TODO, generateMessageMaca continues to store the previous entries
+function generateMessageMaca(mes, dia, precio, nombre, clases, preciocasa, casa, id) {
+
+    const message = `
+    <div class="output-container">
+    <br>
+    <h3>${nombre.toUpperCase()}</h3>
+<p class="texto" id="p-${id}">Buen día ${nombre.charAt(0).toUpperCase()}${nombre.slice(1).toLowerCase()}, ¿cómo estás?
+<br><br>
+Te paso el plan de las clases de ${mes}:<br>
+<br>
+Son *${(parseInt(clases) + parseInt(casa))} ${dia}*, serían *$ ${(parseInt(precio) * parseInt(clases)) + (parseInt(preciocasa) * parseInt(casa))}*<br>
+- En tu casa: $ ${precio * clases} (${precio} x ${clases})<br>
+- En mi casa: $ ${preciocasa * casa} (${preciocasa} x ${casa})<br>
+<br>
+Cualquier cosa avisame, ¡nos vemos el ${dia}!</p>
+    <div class="div-copiar">
+        
+        <svg class="btn-copiar" id="btn-${id}" data-message-id="p-${id}" xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 -960 960 960" width="54"><path class="btn-copiar" id="btn-${id}" data-message-id="p-${id}" d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>
+
+    </div><br>
+</div>
+`;
+    output.innerHTML += message;
+    idCounter++
     return;
 }
 
@@ -109,32 +164,21 @@ function copyMessageToClipboard(elementId) {
 btnBorrar.addEventListener('click', (e) => {
     e.preventDefault();
 
+    inputMesElement.value = '';
+    inputDiaElement.value = '';
     inputNombreElement.value = '';
     inputPrecioElement.value = '';
+    inputClasesElement.value = '';
+
+    inputPrecioCasaElement.value = '';
+    inputCasaElement.value = '';
+
+    inputCasaElement.style.display = 'none';
+    inputPrecioCasaElement.style.display = 'none';
+
+
     total.textContent = '0';
     output.innerHTML = '';
 
     idCounter = 1;
 });
-
-
-`
-Buen día Javi, ¿cómo estás?
-
-Te paso el plan de las clases de *marzo*:
-Son *4 martes*, serían *$ 28000* ($ 7000 x 4).
-
-Cualquier cosa avisame, ¡nos vemos el martes!
-
----------------------------------------------
-
-Buen día Maca, ¿cómo estás?
-
-Te paso el plan de las clases de *marzo*:
-Son *4 jueves*, serían *$ 28000* 
-- En tu casa: $ 16000 (8000 x 2)
-- En mi casa: $ 12000 (6000 x 2)
-
-Cualquier cosa avisame, ¡nos vemos el jueves!
-
-`
